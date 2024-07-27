0b94d58f987a273752eb37105828f3030bd95ac7
MK-TECH-CODES <mukeshkumar14000@gmail.com>
2024-07-27 11:50:36 +0530

    Environment

---
+++ index.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+console.log("Hello  world");
+
+// process.exit(0);
+// process.exitCode = 1;
+
+// Accessing Env
+console.log(process.env.NAME);
+
